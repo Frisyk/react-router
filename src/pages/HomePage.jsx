863e0e00b2a9cc0e3920abef733a1126335e28fc
frisyk <frisnadi1@gmail.com>
2023-02-08 10:10:16 +0700

    add button component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,42 @@
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Button from "../components/Button";
 
 const HomePage = () => {
-    const username = 'Frisnadi'
-   return (
-    <div>
-        <Header />
-        <div className="container">
-            <h1>Hi, {username}</h1>
-            <h2>Have a nice day</h2>
+    const [values, setValues] = useState([])
+    
+    const sayhi = (e) => {
+        console.log(e)
+    }
+    
+    useEffect(() => {
+        axios.get('http://localhost:3004/posts')
+        .then(res => {
+            console.log(res);
+            setValues(res.data);
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                {
+                    values.map((value) => (
+                        <div key={value.id} className="card">
+                            <h1 className="card-header">{value.title}</h1>
+                            <p>{value.comment}</p>
+                            <Button value='Remove' onClick={sayhi}/>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
-        <Footer />
-    </div>
-   ); 
+    ); 
 }
 
 export default HomePage;
